c813e9d3bff72f495ba8a0bc523536c37a6b40d8
cuzimfab@web.de <Lollollol2>
2024-03-24 23:02:17 +0100

    downgraded succesfull

--- fabric-mod/src/main/java/de/michiruf/proxycommand/fabric/ProxyCommandMod.java
+++ fabric-mod/src/main/java/de/michiruf/proxycommand/fabric/ProxyCommandMod.java
@@ -2,11 +2,12 @@ package de.michiruf.proxycommand.fabric;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import de.michiruf.proxycommand.common.ProxyCommandConstants;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.command.CommandManager;
@@ -27,36 +28,37 @@ public class ProxyCommandMod implements ModInitializer {
     @Override
     public void onInitialize() {
         LOGGER.info("ProxyCommand is active");
-        CommandRegistrationCallback.EVENT.register((dispatcher, registry, environment) -> registerCommand(dispatcher));
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            if (dedicated) { // Only register command on dedicated server
+                registerCommand(dispatcher);
+            }
+        });
     }
 
     private static void registerCommand(CommandDispatcher<ServerCommandSource> dispatcher) {
-        LiteralCommandNode<ServerCommandSource> proxyCommand = CommandManager
-                .literal("proxycommand")
-                .requires(cmd -> cmd.hasPermissionLevel(2))
+        dispatcher.register(LiteralArgumentBuilder.<ServerCommandSource>literal("proxycommand")
+                .requires(source -> source.hasPermissionLevel(2))
                 .then(CommandManager.argument("command", StringArgumentType.string())
-                        .executes(ProxyCommandMod::sendMessage)
-                        .build())
-                .build();
-        dispatcher.getRoot().addChild(proxyCommand);
+                        .executes(ProxyCommandMod::sendMessage))
+        );
     }
 
     private static int sendMessage(CommandContext<ServerCommandSource> context) {
-        var command = StringArgumentType.getString(context, "command");
+        String command = StringArgumentType.getString(context, "command");
 
-        var player = context.getSource().getPlayer();
-        if (player == null) {
-            LOGGER.warn("Command \"" + command + "\" was executed without the player as source");
-            context.getSource().sendMessage(Text.literal("Command source must be a player"));
-            return -1;
+        try {
+            var player = context.getSource().getPlayer();
+            // To communicate with the proxy, a S2C packet sent via the player's connection is needed
+            ServerPlayNetworking.send(
+                    player,
+                    new Identifier(ProxyCommandConstants.COMMAND_PACKET_ID),
+                    PacketByteBufs.create().writeString(command)
+            );
+        } catch (Exception e) {
+            LOGGER.warn("Command \"" + command + "\" was executed without a player as source");
+            context.getSource().sendError(Text.of("Command source must be a player"));
+            return 0; // Return 0 to indicate that the command execution was not successful
         }
-
-        // To communicate with the proxy, a S2C packet sent via the players connection is needed (which
-        // is the connection to the proxy indeed)
-        ServerPlayNetworking.send(
-                player,
-                new Identifier(ProxyCommandConstants.COMMAND_PACKET_ID),
-                PacketByteBufs.create().writeString(command));
-        return 1;
+        return 1; // Return 1 to indicate successful command execution
     }
 }
